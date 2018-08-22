bcab7223b04101f0d11b617a9d569e06fbb347d0
Francisco Guimarães <francisco.cpg@gmail.com>
2018-08-22 11:25:43 -0300

    Fix description

--- src/pkg1/file2.test.js
+++ src/pkg1/file2.test.js
@@ -9,7 +9,7 @@ describe('File2 tests', () => {
       });
     });
 
-    describe('when a greater 1', () => {
+    describe('when a greater than 1', () => {
       it('should return 2', () => {
         const ret = funcPkg1File2(2);
         expect(ret).toBe(2);
