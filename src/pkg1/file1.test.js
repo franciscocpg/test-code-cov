16e666099352fee6d860944e5f30b70905eee809
Francisco Guimarães <francisco.cpg@gmail.com>
2018-08-22 09:46:18 -0300

    Decrease code coverage

--- src/pkg1/file1.test.js
+++ src/pkg1/file1.test.js
@@ -2,13 +2,6 @@ const funcPkg1File1 = require('./file1').module;
 
 describe('File1 tests', () => {
   describe('#funcPkg1File1', () => {
-    describe('when a equals 0', () => {
-      it('should return 0', () => {
-        const ret = funcPkg1File1(0);
-        expect(ret).toBe(0);
-      });
-    });
-
     describe('when a greater than 1', () => {
       it('should return 2', () => {
         const ret = funcPkg1File1(4);
