a23e5e2158d022344eb86693b34402c226d94352
Francisco Guimarães <francisco.cpg@gmail.com>
2018-08-22 09:32:13 -0300

    Increase code coverage (#2)

--- src/pkg1/file2.test.js
+++ src/pkg1/file2.test.js
@@ -15,5 +15,12 @@ describe('File2 tests', () => {
         expect(ret).toBe(1);
       });
     });
+
+    describe('when a greater 1', () => {
+      it('should return 2', () => {
+        const ret = funcPkg1File2(2);
+        expect(ret).toBe(2);
+      });
+    });
   });
 });
