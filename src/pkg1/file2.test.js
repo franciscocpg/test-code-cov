287421d260445e0a1a83d9f469c4455ced688a59
Francisco Guimarães <francisco.cpg@gmail.com>
2018-08-22 11:15:14 -0300

    Increase coverage

--- src/pkg1/file2.test.js
+++ src/pkg1/file2.test.js
@@ -2,6 +2,13 @@ const funcPkg1File2 = require('./file2').module;
 
 describe('File2 tests', () => {
   describe('#funcPkg1File2', () => {
+    describe('when a equals 0', () => {
+      it('should return 0', () => {
+        const ret = funcPkg2File1(2);
+        expect(ret).toBe(2);
+      });
+    });
+
     describe('when a greater 1', () => {
       it('should return 2', () => {
         const ret = funcPkg1File2(2);
